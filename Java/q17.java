0e4ed77d8b7a20726a97b879d2478be50562462d
nioomeee <nioomeee@gmail.com>
2025-04-10 19:10:04 +0530

    printing prime nos from 50 to 100

---
+++ Java/q17.java
@@ -0,0 +1,29 @@
+// Write a Java Program to display all prime odd numbers from 50 to 100.
+
+class Solution {
+    public void display() {
+        
+        for(int i = 50; i <= 100; i ++) {
+            if(isPrime(i)){
+                System.out.print(i + " ");
+            } 
+        }
+    }
+
+    boolean isPrime(int num) {
+        for(int i = 2; i < num / 2; i ++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class q17 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        sol.display();
+    }
+}
