0fdbb6530462191d6be7a97e53533609a57a9dd6
nioomeee <nioomeee@gmail.com>
2025-04-14 15:02:26 +0530

    sum od diagonal elements

---
+++ DS/assignment 1/10.cpp
@@ -0,0 +1,51 @@
+// Write a CPP program to sum all the diagonal element.
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+
+};
+
+int main(){
+    
+    int rows, columns;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    cout << "Enter the number of columns: ";
+    cin >> columns;
+
+    if(rows != columns) {
+        cout << "Square matrix required! " << endl;
+        return 1;
+    }
+
+    int matrix[rows][columns];
+
+    cout << "For inputting all the elements: "<<endl;
+
+    int sum = 0;
+    for(int i = 0; i < rows; i ++) {
+        for(int j = 0; j < columns; j ++) {
+            cout << "matrix[" << i << "][" << j << "] : ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "the entered elements are: "<< endl;
+
+    for(int i = 0; i < rows; i ++) {
+        for(int j= 0; j < columns; j ++) {
+            cout << matrix[i][j] << " ";
+            if(i == j) {
+                sum += matrix[i][j];
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "The summ of the diagonal elements = " << sum;
+    cout<<endl;
+    return 0;
+}
