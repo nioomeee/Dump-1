89eb2b59d30636216c02a5f738ed902b0b5c22fd
nioomeee <nioomeee@gmail.com>
2025-04-14 23:43:25 +0530

    Floyd's cycle detection

---
+++ DS/assignment 2/14.cpp
@@ -0,0 +1,44 @@
+// Write a CPP program to check whether the linkedlist is circular or not.
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head; 
+    Node* tail;
+
+
+    bool isCircular() {
+        Node* slow = head;
+        Node* fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main(){
+    
+    
+    cout<<endl;
+    return 0;
+}
